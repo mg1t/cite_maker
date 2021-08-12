8aa869f5d0a86e40d3f55e5191985a85b14e3a5d
mg1t <75778885+mg1t@users.noreply.github.com>
2021-08-12 20:58:48 +0200

    Initialisierung

---
+++ install.bat
@@ -0,0 +1,19 @@
+@ECHO off
+title APRS32 Commander Package Installer
+pip3 install --upgrade pip
+pip3 install pysimplegui
+pip3 install numpy
+pip3 install matplotlib
+pip install remi
+pip install pysimpleguiweb
+cls
+
+ECHO ########################################
+ECHO #  APRS32 Commander Package Installer  #
+ECHO ########################################
+ECHO #       Alle benoetigten Dateien       #
+ECHO #          wurden installiert          #
+ECHO #                                      #
+ECHO #       Druecke ENTER zum beenden      #
+ECHO ########################################
+pause >nul
